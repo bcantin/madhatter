57dd084e1762bc8492a1c39ef9519a8e16ccd181
Brad Cantin <brad.cantin@threattrack.com>
2013-04-28 16:49:32 -0500

    Update to files for searching

--- app/controllers/hats_controller.rb
+++ app/controllers/hats_controller.rb
@@ -1,7 +1,15 @@
 class HatsController < ApplicationController
 
   def index
-    @hats = Hat.limit(10)
+    params[:style] ||= []
+    params[:color] ||= []
+    @hats = Hat.searchable(params)
+
+    @hat_styles = {}
+    @hats.facets["current-styles"]["terms"].each {|f| @hat_styles[f['term']] = f['count']}
+
+    @hat_colors = {}
+    @hats.facets["global-colors"]["terms"].each {|f| @hat_colors[f['term']] = f['count']}
   end
 
 end
--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,4 +1,7 @@
 class Hat < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+  
   attr_accessible :name, :price, :color_list, :style_list, :description
 
   acts_as_taggable_on :colors, :styles
@@ -6,5 +9,24 @@ class Hat < ActiveRecord::Base
   STYLES =  %w{baseball beanie beret derby cricket deerstalker 
                dunce fedora fez patrol sombereo stetson top zucchetto}
   COLORS = %w{brown black white grey tan blue red green orange}
+
+  def self.searchable(opts={})
+    @hats = Hat.search(page: opts[:page] || 1) do
+      query { string opts[:search] }           if opts[:search].present?
+      filter(:terms, style_list: opts[:style]) if opts[:style].present?
+      filter(:terms, color_list: opts[:color]) if opts[:color].present?
+      facet "current-styles" do
+        terms :style_list, size: STYLES.size
+      end        
+    end
+  end
+
+  def to_indexed_json
+    { name: name,
+      description: description,
+      price: price,
+      color_list: color_list,
+      style_list: style_list}.to_json
+  end
 
 end
